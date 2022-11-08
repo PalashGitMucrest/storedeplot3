30834c03807df639520b9d0a6356bd3f2a23bd0b
PalashGitMucrest <palash.g@mucrest.com>
2022-11-08 15:20:55 +0530

    Updates

---
+++ precache-manifest.87d0a0678eb56576e8c2fcefc25e0ea4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6ca2468d82b9192c4145e607369c79",
+    "url": "/storedeplot3/index.html"
+  },
+  {
+    "revision": "c773d9fd4d2103e172a6",
+    "url": "/storedeplot3/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "941c1a8b272d75bc1614",
+    "url": "/storedeplot3/static/css/main.73f571ab.chunk.css"
+  },
+  {
+    "revision": "c773d9fd4d2103e172a6",
+    "url": "/storedeplot3/static/js/2.99883411.chunk.js"
+  },
+  {
+    "revision": "8dccb21facd929c60dbdc534cd0c42dc",
+    "url": "/storedeplot3/static/js/2.99883411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe3ffbb2bb97b2dc5032",
+    "url": "/storedeplot3/static/js/3.b8667fcc.chunk.js"
+  },
+  {
+    "revision": "941c1a8b272d75bc1614",
+    "url": "/storedeplot3/static/js/main.4bcee4b8.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/storedeplot3/static/js/main.4bcee4b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe2f07398e40e10dada",
+    "url": "/storedeplot3/static/js/runtime-main.395e6e39.js"
+  }
+]);
